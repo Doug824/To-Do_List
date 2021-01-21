742b3ab2c5002de53024d49ee9cd575a3d3806b2
Doug Hagan <doug.hagan824@gmail.com>
2021-01-21 11:15:39 -0500

    push to github

---
+++ app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('#add-item');
+const input = document.querySelector('#item');
+const toDoList = document.querySelector('#todo-list');
+
+toDoList.addEventListener('click', function(e) {
+    if(e.target.tagName === 'BUTTON') {
+        e.target.parentElement.remove();
+    } 
+    else if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('most-important');
+    }
+});
+
+toDoList.addEventListener('dblclick', function(e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('completed-not-deleted')
+    }
+});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const newToDo = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'X';
+
+    newToDo.innerText = input.value + " ";
+    newToDo.appendChild(removeBtn);
+    toDoList.appendChild(newToDo);
+});
